b3a1aa36e0aad3b642f6a10c0701a2f1f6fc45d5
Jackson <635725626@qq.com>
2021-03-28 14:21:18 +0800

    roberta baseline add lr monitor

--- run_baseline_roberta.py
+++ run_baseline_roberta.py
@@ -127,7 +127,7 @@ def train(model, tokenizer, checkpoint):
     for epoch in range(checkpoint, args.epochs):
         model.train()
         epoch_loss = []
-
+        step = 0
         for batch in tqdm(train_dataLoader, desc="Iteration"):
             model.zero_grad()
             # 设置tensor gpu运行
@@ -150,7 +150,10 @@ def train(model, tokenizer, checkpoint):
 
             optimizer.step()
             scheduler.step()
-
+            step += 1
+            if step % 500 == 0:
+              logger.debug("loss:"+str(np.array(epoch_loss).mean()))
+              logger.debug('learning_rate:' + str(optimizer.state_dict()['param_groups'][0]['lr']))
             # 保存模型
         output_dir = args.save_dir + "/checkpoint-" + str(epoch)
         if not os.path.exists(output_dir):
