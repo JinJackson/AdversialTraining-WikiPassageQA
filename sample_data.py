de0d375d25a84866dcb030ea28c2e72bc96fb40b
Jackson <635725626@qq.com>
2021-04-18 20:58:30 +0800

    split data

---
+++ sample_data.py
@@ -0,0 +1,21 @@
+import random
+
+with open('data/wikipassageQA/text/train.txt', 'r', encoding='utf-8') as reader:
+    lines = reader.readlines()
+with open('data/AttackedText/rate1_pos_text.txt', 'r', encoding='utf-8') as reader:
+    lines_attack = reader.readlines()
+
+length = len(lines)
+
+nums_list = list(range(length))
+
+shuffle_selected = random.sample(nums_list, 40000)
+
+with open('data/sample_data/origin/train.txt', 'w', encoding='utf-8') as writer:
+    for index in shuffle_selected:
+        writer.write(lines[index].strip() + '\n')
+
+
+with open('data/sample_data/attack/train_attack.txt', 'w', encoding='utf-8') as writer:
+    for index in shuffle_selected:
+        writer.write(lines_attack[index].strip() + '\n')
---
+++ splitData.py
@@ -0,0 +1,20 @@
+data_file = 'data/sample_data/origin/train.txt'
+with open(data_file, 'r', encoding='utf-8') as reader:
+    lines = reader.readlines()
+
+length = len(lines)
+
+splits = 8
+assert length % splits == 0
+
+file_size = int(length / splits)
+
+count = 0
+for i in range(1, splits+1):
+    written_file = 'data/sample_data/origin/train' + str(i) + '.txt'
+    with open(written_file, 'w', encoding='utf-8') as writer:
+        start = count
+        print(start)
+        for j in range(start, i*file_size):
+            writer.write(lines[j].strip() + '\n')
+            count += 1
