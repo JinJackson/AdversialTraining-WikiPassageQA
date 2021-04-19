f6666e210b8530fa07a43e6d8d169cf7cdd8bc00
Jackson <635725626@qq.com>
2021-04-19 13:48:16 +0800

    sample train

---
+++ all_datasets/SampleAttackDataset.py
@@ -0,0 +1,89 @@
+import numpy as np
+import json, codecs
+from torch.utils.data import Dataset
+
+
+from transformers import RobertaTokenizer
+from torch.utils.data import DataLoader
+
+
+def getOriginDatas(data_file):
+    all_datas = []
+    with open(data_file, 'r', encoding='utf-8') as reader:
+        lines = reader.readlines()
+        for line in lines:
+            query, answer, label = line.strip().split('\t')
+            all_datas.append([query, answer, label])
+    return all_datas
+
+def getAttackedData(attackedFile):
+    attackedData = []
+    with open(attackedFile, 'r', encoding='utf-8') as reader:
+        lines = reader.readlines()
+        for line in lines:
+            query, answer, label = line.strip().split('  [SEP]  ')
+            label = int(label)
+            attackedData.append([query, answer, label])
+
+    return attackedData
+
+#这个dataset类只用作训练，BertDataset中的TrainData可以用来加载dev和test，这个dataset没有记录docs之类的，无法用于dev和test
+class AttackTrainData(Dataset):
+    def __init__(self, data_file, tokenizer, max_length, attacked_file=None):
+        pairs = getOriginDatas(data_file)
+        if attacked_file:
+            attackedData = getAttackedData(attacked_file)
+        else:
+            attackedData = None
+        self.tokenizer = tokenizer
+        self.attackedData = attackedData
+        self.pairs = pairs
+        self.max_length = max_length
+    def __getitem__(self, index):
+        data = self.pairs[index]
+        query, answer, label = data
+        tokenized_dic = self.tokenizer.encode_plus(text=query,
+                                          text_pair=answer,
+                                          truncation=True,
+                                          padding='max_length',
+                                          max_length=self.max_length)
+        if self.attackedData:
+            adv_data = self.attackedData[index]
+            adv_query, adv_answer, adv_label = adv_data
+            adv_tokenized_dic = self.tokenizer.encode_plus(text=adv_query,
+                                                           text_pair=adv_answer,
+                                                           truncation=True,
+                                                           padding='max_length',
+                                                           max_length=self.max_length)
+
+            return [np.array(tokenized_dic['input_ids']), np.array(tokenized_dic['token_type_ids']), np.array(tokenized_dic['attention_mask']), np.array([data[2]])], \
+                   [np.array(adv_tokenized_dic['input_ids']), np.array(tokenized_dic['token_type_ids']), np.array(adv_tokenized_dic['attention_mask']), np.array([adv_data[2]])]
+
+        return [np.array(tokenized_dic['input_ids']), np.array(tokenized_dic['token_type_ids']), np.array(tokenized_dic['attention_mask']), np.array([data[2]])]
+
+    def __len__(self):
+        # You should change 0 to the total size of your all_datasets.
+        return len(self.pairs)
+
+# if __name__ == '__main__':
+#     from transformers import BertTokenizer
+#     train_file = '../data/wikipassageQA/train.tsv'
+#     doc_file = '../data/wikipassageQA/document_passages.json'
+#     max_length = 400
+#     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+#     batch_size = 2
+#     shuffle = True
+#     train_data = TrainData(data_file=train_file,
+#                           doc_file=doc_file,
+#                           max_length=max_length,
+#                           tokenizer=tokenizer,
+#                           attacked_file=None
+#                           )
+#     train_dataLoader = DataLoader(dataset=train_data,
+#                                 batch_size=batch_size,
+#                                 shuffle= shuffle)
+#
+#
+#     for batch in train_dataLoader:
+#         print(batch)
+#         break
--- run_BertSampleTrain.py
+++ run_BertSampleTrain.py
@@ -1,28 +1,23 @@
 #coding=utf-8
 
-#params:
-#--do_train --bert_model  "bert-base-uncased" --model_type "MatchModel" --train_file "data/wikipassageQA/train.tsv" --dev_file "data/wikipassageQA/dev.tsv" --test_file "data/wikipassageQA/test.tsv" --do_lower_case --learning_rate 2e-6 --gpu 0 --epochs 5 --batch_size 2 --accumulate 1 --max_length 100 --seed 1024 --shuffle True --save_dir "result/test"
 
-'''
---do_train \
---bert_model  "bert-base-uncased" \
---model_type "MatchModel" \
---train_file "data/wikipassageQA/train.tsv" \
---dev_file "data/wikipassageQA/dev.tsv" \
---test_file "data/wikipassageQA/test.tsv" \
---do_lower_case \
---learning_rate 2e-6 \
---gpu 0 \
---epochs 5 \
---batch_size 2 \
---accumulate 1 \
---max_length 100 \
---seed 1024 \
---shuffle True \
---save_dir "result/test"
-'''
-
-
+#--do_train \
+# --bert_model  "bert-base-uncased" \
+# --model_type "MatchModel" \
+# --train_file "data/wikipassageQA/train.tsv" \
+# --dev_file "data/wikipassageQA/dev.tsv" \
+# --test_file "data/wikipassageQA/test.tsv" \
+# --attacked_file "data/AttackedText/rate1_pos_text.txt"
+# --do_lower_case \
+# --learning_rate 2e-6 \
+# --gpu 0 \
+# --epochs 5 \
+# --batch_size 2 \
+# --accumulate 1 \
+# --max_length 100 \
+# --shuffle
+# --seed 1024 \
+# --save_dir "result/test"
 from parser1 import args
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer, AdamW, get_linear_schedule_with_warmup
@@ -33,11 +28,17 @@ import torch
 import numpy as np
 from tqdm import tqdm
 
+# from all_datasets import DataBert
+# from AttackDataset import AttackedData
+
 from all_datasets.BertDataset import TrainData
-from all_datasets.SampleBaselineDataset import SampleTrainData
+from all_datasets.SampleAttackDataset import AttackTrainData
 
 from utils.metrics import mrr, map
 from utils.logger import getLogger
+
+from utils.FGM import FGM
+
 
 def seed_torch(seed):
     random.seed(seed)
@@ -57,6 +58,7 @@ if args.seed > -1:
 model_name = 'model.' + args.model_type
 BertMatchModel = __import__(model_name, globals(), locals(), [args.model_type]).BertMatchModel
 
+loss_rate = args.loss_rate
 logger = None
 doc_file='data/wikipassageQA/document_passages.json'
 
@@ -70,17 +72,20 @@ def train(model, tokenizer, checkpoint):
     else:
         amp = None
     # 训练数据处理
-    train_data = SampleTrainData(data_file=args.train_file,
-                          max_length=args.max_length,
-                          tokenizer=tokenizer
+    train_data = AttackTrainData(data_file=args.train_file,
+                          tokenizer=tokenizer,
+                           max_length=args.max_length,
+                           attacked_file=args.attacked_file
                           )
     train_dataLoader = DataLoader(dataset=train_data,
                                 batch_size=args.batch_size,
                                 shuffle= args.shuffle)
 
 
+    print('train_data:', len(train_data))
     # 初始化 optimizer，scheduler
     t_total = len(train_dataLoader) * args.epochs
+    num_warmup_steps = int(args.warmup_steps * t_total)
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
         {
@@ -89,7 +94,6 @@ def train(model, tokenizer, checkpoint):
         },
         {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
     ]
-    num_warmup_steps = args.warmup_steps * t_total
     optimizer = AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = get_linear_schedule_with_warmup(
         optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=t_total
@@ -109,11 +113,11 @@ def train(model, tokenizer, checkpoint):
 
     # 开始训练
     logger.debug("***** Running training *****")
-    logger.debug("  Num Steps = %d", len(train_dataLoader))
+    logger.debug("  Num examples = %d", len(train_dataLoader))
     logger.debug("  Num Epochs = %d", args.epochs)
     logger.debug("  Set_Batch size = %d", args.batch_size)
     logger.debug("  Real_Batch_size = %d", args.batch_size * args.accumulate)
-    #logger.debug("  Loss_rate_ = " + str(args.loss_rate))
+    logger.debug("  Loss_rate_ = " + str(args.loss_rate))
     logger.debug("  Shuffle = " + str(args.shuffle))
     logger.debug("  warmup_steps = " + str(num_warmup_steps))
 
@@ -126,11 +130,13 @@ def train(model, tokenizer, checkpoint):
     for epoch in range(checkpoint, args.epochs):
         model.train()
         epoch_loss = []
+
         step = 0
-        for batch in tqdm(train_dataLoader, desc="Iteration"):
-            model.zero_grad()
+        attack_batch_count = 0
+        for batch, batch_attack in tqdm(train_dataLoader, desc="Iteration", total=len(train_dataLoader)):
             # 设置tensor gpu运行
-            batch = tuple(t.to(args.device) for t in batch)
+            model.zero_grad()
+            batch = tuple(t.to('cuda') for t in batch)
             input_ids, token_type_ids, attention_mask, labels = batch
 
             outputs = model(input_ids=input_ids.long(),
@@ -138,9 +144,20 @@ def train(model, tokenizer, checkpoint):
                             attention_mask=attention_mask,
                             labels=labels)
 
-            loss = outputs[0]
+            loss_clean = outputs[0]
 
-            epoch_loss.append(loss.item())
+            batch_attack = tuple(t.to('cuda') for t in batch_attack)
+
+            input_ids2, token_type_ids2, attention_mask2, labels2 = batch_attack
+
+
+            outputs_attack = model(input_ids=input_ids2.long(),
+                                   token_type_ids=token_type_ids2.long(),
+                                   attention_mask=attention_mask2,
+                                   labels=labels2)
+            loss_adv = outputs_attack[0]
+
+            loss = loss_clean + loss_adv
 
             if args.fp16:
                 with amp.scale_loss(loss, optimizer) as scaled_loss:
@@ -148,12 +165,18 @@ def train(model, tokenizer, checkpoint):
             else:
                 loss.backward()
 
+            epoch_loss.append(loss.item())
+
+            fgm.restore()
+
             optimizer.step()
             scheduler.step()
+
             step += 1
             if step % 500 == 0:
               logger.debug("loss:"+str(np.array(epoch_loss).mean()))
               logger.debug('learning_rate:' + str(optimizer.state_dict()['param_groups'][0]['lr']))
+
             # 保存模型
         output_dir = args.save_dir + "/checkpoint-" + str(epoch)
         if not os.path.exists(output_dir):
@@ -168,7 +191,7 @@ def train(model, tokenizer, checkpoint):
         torch.save(optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
         torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
         logger.debug("Saving optimizer and scheduler states to %s", output_dir)
-
+        logger.debug("Use Attack Example Batch:" + str(attack_batch_count))
         # eval dev
         eval_loss, eval_map, eval_mrr = evaluate(model, tokenizer, eval_file=args.dev_file,
                                                 checkpoint=epoch,
@@ -190,7 +213,7 @@ def evaluate(model, tokenizer, eval_file, checkpoint, output_dir=None):
                          doc_file=doc_file,
                          max_length=args.max_length,
                          tokenizer=tokenizer,
-                          attacked_file=None)
+                         attacked_file=None)
 
     eval_dataLoader = DataLoader(dataset=eval_data,
                                 batch_size=args.batch_size,
@@ -211,7 +234,7 @@ def evaluate(model, tokenizer, eval_file, checkpoint, output_dir=None):
     model.eval()
 
     for batch in tqdm(eval_dataLoader, desc="Evaluating"):
-        batch = tuple(t.to(args.device) for t in batch)
+        batch = tuple(t.to('cuda') for t in batch[:4])
         input_ids, token_type_ids, attention_mask, labels = batch
 
         with torch.no_grad():
@@ -261,12 +284,12 @@ if __name__ == "__main__":
                 pass
         checkpoint_dir = args.save_dir + "/checkpoint-" + str(checkpoint)
         if checkpoint > -1:
-            logger.debug(f" Load Model from {checkpoint_dir}")
+            logger.debug("Load Model from {}".format(checkpoint_dir))
         tokenizer = BertTokenizer.from_pretrained(args.bert_model if checkpoint == -1 else checkpoint_dir,
                                                   do_lower_case=args.do_lower_case
                                                   )
         model = BertMatchModel.from_pretrained(args.bert_model if checkpoint == -1 else checkpoint_dir)
-        model.to(args.device)
+        model.to('cuda')
         # 训练
         train(model, tokenizer, checkpoint)
 
@@ -278,7 +301,7 @@ if __name__ == "__main__":
                                                   do_lower_case=args.do_lower_case,
                                                   )
         model = BertMatchModel.from_pretrained(checkpoint_dir)
-        model.to(args.device)
+        model.to('cuda')
         # 评估
         eval_loss, eval_map, eval_mrr = evaluate(model, tokenizer, eval_file=args.test_file,
                                                 checkpoint=checkpoint)
